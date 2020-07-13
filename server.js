5423898f9218bf179929c654c95c46343cb00b41
Gutembergue Mascarenhas <gmascarenhas3001@gmail.com>
2020-07-13 23:51:35 +0200

    remove morgan

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const morgan = require("morgan");
 const PORT = 3001;
 
 const app = express();
 
-app.use(morgan("dev"));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
