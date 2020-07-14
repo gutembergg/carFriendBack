e755042f7a1c529afe9de323dc910fe52cef517e
Gutembergue Mascarenhas <gmascarenhas3001@gmail.com>
2020-07-15 01:27:20 +0200

    ok

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({ origins: "https://car-friend-guto.herokuapp.com" }));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
