409458410df318147e8fa411aa36ef9cba3025ac
Gutembergue Mascarenhas <gmascarenhas3001@gmail.com>
2020-07-15 01:29:03 +0200

    ok

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://car-friend-guto.herokuapp.com");
+    res.header("Access-Control-Allow-Origin", "https://car-friend-guto.herokuapp.com/");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header(
